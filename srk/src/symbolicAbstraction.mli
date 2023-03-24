778da49e56b25896cc24e7c95fd1a4e5146bef66
Nicolas Koh <ckoh@princeton.edu>
2023-03-23 20:17:36 -0400

    Symbolic abstraction interface

--- srk/src/symbolicAbstraction.mli
+++ srk/src/symbolicAbstraction.mli
@@ -2,7 +2,6 @@
 val polyhedral_abs_by_mbp :
   'a Syntax.context -> 'a Syntax.formula -> Syntax.symbol list -> DD.closed DD.t
 
-
 val polyhedral_lattice_abs_by_mbp :
   'a Syntax.context -> 'a Syntax.formula -> Syntax.symbol list ->
   DD.closed DD.t * IntLattice.t

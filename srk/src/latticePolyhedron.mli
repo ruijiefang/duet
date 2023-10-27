40007d2df71c887615bdb3dc2e7deff51f8d67e5
Nicolas Koh <ckoh@princeton.edu>
2023-10-27 10:31:57 -0400

    Comments

--- srk/src/latticePolyhedron.mli
+++ srk/src/latticePolyhedron.mli
@@ -5,46 +5,44 @@
     while the constraints of P are constraints for inequalities.
  *)
 
-(** [mixed_lattice_hull p l] computes the "integer" hull of [p] w.r.t. [l],
-    where [p] need not be in the linear space spanned by [l].
-    The generators of [l] are the generators of the integrality
-    constraints.
+(** [mixed_lattice_hull P L = P'] where
+    [P' = convex.hull(P \cap {x: Int(l^T x) for all l in L})].
  *)
 val mixed_lattice_hull: 'a Syntax.context -> Polyhedron.t -> IntLattice.t ->
                         DD.closed DD.t
 
-(** 
+(**
     Given (P, L) representing a linear arithmetic formula and [elim] a list of
     integer variables, and a point [m] in [P /\ L],
     [local_project_cooper m elim p l] computes (P', L') such that
     (i)   [P'] and [L'] are in dimensions excluding [elim].
     (ii)  [m] is in [P' /\ L'].
-    (iii) There exists terms [ts] such that 
+    (iii) There exists terms [ts] such that
           [formula_of(P') = formula_of(P)[ts/elim]]
           and [formula_of(L') = formula_of(P)[ts/elim]].
     The algorithm is a local projection version of Cooper's algorithm.
     When the linear span of [L] contains all dimensions of [P] (e.g., when all
-    variables are integral), local projection is image-finite (i.e., the 
-    number of terms [ts] over all [m] in [P /\ L] is finite) because of 
+    variables are integral), local projection is image-finite (i.e., the
+    number of terms [ts] over all [m] in [P /\ L] is finite) because of
     Cooper's result.
 
-    In the general case, it is not, and [local_project_cooper] takes 
-    a function [round] that discretizes the greatest lower bound term 
+    In the general case, it is not, and [local_project_cooper] takes
+    a function [round] that discretizes the greatest lower bound term
     (of a variable to be eliminated) to possibly get a finite image.
 
     Precisely, [round] has to be a function satisfying the following conditions.
     If [round ckind lower_bound m = (t, ineqs, ints)],
     - [ineqs /\ IsInt(ints) |= t >= lower_bound].
-    - For all integers n, 
+    - For all integers n,
       [ineqs /\ IsInt(ints) |= n <ckind> lower_bound -> n <ckind> t].
     - [ineqs], [ints], and [t] all involve only dimensions in [lower_bound].
     These ensure soundness, i.e., that (i), (ii) and (iii) hold.
 
     For image-finiteness, [round] needs to satisfy the following.
-    - [round ckind lower_bound] as a function from points of [P /\ L] to 
+    - [round ckind lower_bound] as a function from points of [P /\ L] to
       {(t, ineqs, ints)} has a finite image.
     - When [round ckind lower_bound m = (t, ineqs, ints)],
-      {t(valuation): valuation in Q^{variables in t}} is a subset of 
+      {t(valuation): valuation in Q^{variables in t}} is a subset of
       {rn: n \in ZZ} for some rational [r].
  *)
 val local_project_cooper:
